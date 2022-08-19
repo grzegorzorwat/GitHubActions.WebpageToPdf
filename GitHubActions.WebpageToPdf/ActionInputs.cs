9360b02062bb3c6629f0275f00ef3d9d7e10cb31
grzegorzorwat <grzegorzorwat+github@protonmail.com>
2022-08-19 15:04:48 +0200

    feat: generate pdf from webpage using puppeteer

---
+++ GitHubActions.WebpageToPdf/ActionInputs.cs
@@ -0,0 +1,38 @@
+﻿using CommandLine;
+
+namespace GitHubActions.WebpageToPdf;
+
+public class ActionInputs
+{
+    [Option('a', "address",
+        Required = true,
+        HelpText = "Webpage address to generate PDF from.")]
+    public string WebpageAddress { get; init; } = null!;
+
+    [Option('o', "output",
+        Required = true,
+        HelpText = "Output directory.")]
+    public string OutputDirectory { get; init; } = null!;
+
+    [Option('f', "file-name",
+        Required = true,
+        HelpText = "Output file name.")]
+    public string OutputFileName { get; init; } = null!;
+
+    [Option('m', "append-metadata",
+        Required = false,
+        HelpText = "Append timestamp to file name.")]
+    public bool AppendMetadata { get; init; }
+
+    [Option("media-type-screen",
+        Required = false,
+        HelpText = "Emulate screen media type.",
+        SetName = "MediaTypeScreen")]
+    public bool EmulateScreenMediaType { get; init; }
+
+    [Option("media-type-print",
+        Required = false,
+        HelpText = "Emulate print media type.",
+        SetName = "MediaTypePrint")]
+    public bool EmulatePrintMediaType { get; init; }
+}
--- GitHubActions.WebpageToPdf/Program.cs
+++ GitHubActions.WebpageToPdf/Program.cs
@@ -1,1 +1,62 @@
-﻿Console.WriteLine("");
+﻿using CommandLine;
+using GitHubActions.WebpageToPdf;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using PuppeteerSharp;
+using PuppeteerSharp.Media;
+using static CommandLine.Parser;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) => { })
+    .Build();
+
+static TService Get<TService>(IHost host)
+    where TService : notnull =>
+    host.Services.GetRequiredService<TService>();
+
+var parser = Default.ParseArguments<ActionInputs>(() => new(), args);
+parser.WithNotParsed(
+    errors =>
+    {
+        Get<ILoggerFactory>(host)
+            .CreateLogger(typeof(Program).FullName!)
+            .LogError(
+                string.Join(
+                    Environment.NewLine, errors.Select(error => error.ToString())));
+
+        Environment.Exit(2);
+    });
+
+await parser.WithParsedAsync(options => StartPdfGenerationAsync(options, host));
+await host.RunAsync();
+
+static async Task StartPdfGenerationAsync(ActionInputs inputs, IHost host)
+{
+    var logger = Get<ILoggerFactory>(host).CreateLogger(nameof(StartPdfGenerationAsync));
+    using var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+    {
+        Headless = true,
+        Args = new[] { "--no-sandbox", "--disable-gpu", "--disable-setuid-sandbox", "--disable-dev-shm-usage", "--single-process" },
+        ExecutablePath = "/usr/bin/google-chrome"
+    });
+    await using var page = await browser.NewPageAsync();
+    await page.GoToAsync(inputs.WebpageAddress, WaitUntilNavigation.Networkidle0);
+    await page.EmulateMediaTypeAsync(GetMediaType(inputs));
+
+    var fileName = $"{inputs.OutputFileName}{(inputs.AppendMetadata ? $"_{DateTime.Now:yyyyMMddHHmmss}" : "")}.pdf";
+    var fullPath = Path.Combine(inputs.OutputDirectory, fileName);
+
+    await page.PdfAsync(fullPath);
+    string title = $"Created pdf for webpage {inputs.WebpageAddress}.";
+    Console.WriteLine($"::set-output name=title::{title}");
+
+    Environment.Exit(0);
+}
+
+static MediaType GetMediaType(ActionInputs inputs) =>
+    inputs.EmulateScreenMediaType
+        ? MediaType.Screen
+        : inputs.EmulatePrintMediaType
+            ? MediaType.Print
+            : MediaType.None;
